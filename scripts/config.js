e9587f0f5c4a115694930851cc0c5eb41a09bcca
Ulysses Carbajal <ulyssescarbajal04@gmail.com>
2024-07-06 14:30:07 -0700

    fixing bugs

---
+++ scripts/config.js
@@ -0,0 +1,7 @@
+// config.js
+const CONFIG = {
+    API_BASE_URL:'https://animetize-api-wgiz.onrender.com' || process.env.API_BASE_URL ,
+    // Add other configuration variables here
+};
+
+export default CONFIG;
