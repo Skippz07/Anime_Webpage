0de5ef67d51f2e90bb01954f127991586f4325ec
Ulysses Carbajal <ulyssescarbajal04@gmail.com>
2024-07-07 23:24:55 -0700

    cors access

--- scripts/config.js
+++ scripts/config.js
@@ -1,7 +1,7 @@
 // config.js
 const CONFIG = {
-    //API_BASE_URL:'https://animetize-api-wgiz.onrender.com'  || process.env.API_BASE_URL ,
-    API_BASE_URL:'https://animetesting.vercel.app/' ,
+    API_BASE_URL:'https://animetize-api-wgiz.onrender.com' ,
+    //API_BASE_URL:'https://animeapidemov1.vercel.app/' || process.env.API_BASE_URL ,
 
     // Add other configuration variables here
 };
