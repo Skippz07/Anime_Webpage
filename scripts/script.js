933a992b9ffd9151074444e076f6756f6dfa9843
Ulysses Carbajal <ulyssescarbajal04@gmail.com>
2024-07-08 00:15:55 -0700

    removed search icon and improved layout

--- scripts/script.js
+++ scripts/script.js
@@ -314,5 +314,5 @@ document.addEventListener('DOMContentLoaded', async () => {
         document.getElementById('search-input').addEventListener('input', handleSearchInput);
         // Hide the loading screen after content is loaded
         document.getElementById('loading-screen').style.display = 'none';
-    }, 3000); // Adjust this value for longer/shorter duration
+    }, 2000); // Adjust this value for longer/shorter duration
 });
